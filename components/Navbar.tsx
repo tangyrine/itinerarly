bd008a29ef388bc28b3976c22997c8ff7213f89e
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-05-15 22:03:45 +0530

    fix: tailwind usuage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Navbar: React.FC = () => (
-  <nav className="bg-blue-600 p-4">
+  <nav className="bg-gray-800 p-4">
     <div className="container mx-auto">
       <h1 className="text-white text-xl font-bold">Itinerary App</h1>
     </div>
