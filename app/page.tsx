21e13b21eaafc3a91795474a5267ec0a02ff97e3
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-05-19 22:07:10 +0530

    Implement code changes to enhance functionality and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Body from "@/components/Body";
 import Footer from "@/components/Footer";
@@ -9,7 +9,15 @@ const Page: React.FC = () => {
   return (
     <div>
       {/* Video background container for Navbar and Body */}
-      <div className="relative min-h-screen flex flex-col">
+      <div
+        className="relative min-h-screen flex flex-col"
+        style={{
+          backgroundColor: "##4d6b68", 
+          backgroundImage: "url(/assets/bg-poster.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <video
           className="absolute inset-0 w-full h-full object-cover"
           autoPlay
@@ -17,11 +25,12 @@ const Page: React.FC = () => {
           muted
           playsInline
           preload="auto"
+          poster="/assets/bg-poster.png"
           src="/assets/background.mp4"
         >
           Your browser does not support the video tag.
         </video>
-        
+
         {/* Overlay content */}
         <div className="relative z-10 flex flex-col min-h-screen">
           <Navbar />
